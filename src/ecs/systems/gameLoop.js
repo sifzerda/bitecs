60bc0745fb4db4e094a11dbe39ba5d7155b81aa5
Troygit config --global user.email <tydamon@hotmail.com>
2026-06-28 19:56:21 +1000

    ...

--- src/ecs/systems/gameLoop.js
+++ src/ecs/systems/gameLoop.js
@@ -1,33 +1,19 @@
 // src/ecs/systems/gameLoop.js
 
-
 import playerControlSystem from "./playerControlSystem.js"
 
 import { movementSystem } from "./movement.js"
 import { boundsSystem } from "./bounds.js"
 import { combatSystem } from "./combat.js"
 
-
-
 export function gameLoop(
     keys,
     shootState
 ){
 
-
-    playerControlSystem(
-        keys,
-        shootState
-    )
-
-
+    playerControlSystem(keys, shootState)
     movementSystem()
-
-
     boundsSystem()
-
-
     combatSystem()
-
 
 }
