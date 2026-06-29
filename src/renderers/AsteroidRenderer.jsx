992e7609e41b14dda791ccf0d8238fd014c5fd20
Troygit config --global user.email <tydamon@hotmail.com>
2026-06-29 22:12:31 +1000

    ...

--- src/renderers/AsteroidRenderer.jsx
+++ src/renderers/AsteroidRenderer.jsx
@@ -3,7 +3,6 @@
 import { useMemo, useRef } from 'react'
 import { useFrame } from '@react-three/fiber'
 import * as THREE from 'three'
-
 import { asteroidQuery } from '../ecs/queries.js'
 import { world } from '../ecs/world.js'
 import { Position, Health } from '../ecs/components.js'
