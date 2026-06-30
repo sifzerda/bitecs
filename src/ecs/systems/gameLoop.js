ef19c68967e70a9bc4d846bdf059cb97aaf31376
Troygit config --global user.email <tydamon@hotmail.com>
2026-06-30 22:17:11 +1000

    ufo shoots

--- src/ecs/systems/gameLoop.js
+++ src/ecs/systems/gameLoop.js
@@ -7,23 +7,17 @@ import { combatSystem } from "./combat.js"
 import { difficultySystem } from "./difficultySystem.js"
 import { spawnDirectorSystem } from "./spawnDirector.js"
 import { ufoMovementSystem } from "./ufoMovementSystem.js"
+import { ufoShootSystem } from "./ufoShootSystem.js"
 
 export function gameLoop(keys, shootState) {
-
-    //-------------------------------------
-    // Update progression
-    //-------------------------------------
 
     difficultySystem()
     spawnDirectorSystem()
 
-    //-------------------------------------
-    // Gameplay
-    //-------------------------------------
-
     playerControlSystem(keys, shootState)
     movementSystem()
     ufoMovementSystem()
+    ufoShootSystem()
     boundsSystem()
     combatSystem()
 }
