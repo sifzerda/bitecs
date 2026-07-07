f2c677b414c2f714e3d33d8ae2a08408da6424be
Troygit config --global user.email <tydamon@hotmail.com>
2026-07-07 12:31:19 +1000

    enhanced bulletrenderer

--- src/ecs/pools/bulletPool.js
+++ src/ecs/pools/bulletPool.js
@@ -1,1 +1,11 @@
 // src/ecs/pools/bulletPool.js
+
+const pool = [];
+
+export function acquireBullet() {
+  return pool.pop() || {};
+}
+
+export function releaseBullet(bullet) {
+  pool.push(bullet);
+}
