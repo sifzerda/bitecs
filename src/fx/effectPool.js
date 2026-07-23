d7b04eb64b3a1278dd10be2ae4afb77a4347ab40
Troygit config --global user.email <tydamon@hotmail.com>
2026-07-23 18:38:47 +1000

    ...

--- src/fx/effectPool.js
+++ src/fx/effectPool.js
@@ -1,10 +1,6 @@
 // src/fx/effectPool.js
 
-
-export function createTypedEffectPool(
-    capacity,
-    fields = []
-) {
+export function createTypedEffectPool(capacity, fields = []) {
 
     const pool = {
 
@@ -18,52 +14,40 @@ export function createTypedEffectPool(
         life: new Float32Array(capacity),
         maxLife: new Float32Array(capacity),
 
-        cursor: 0,
+        cursor: 0
 
+    }
 
-        allocate() {
+    pool.allocate = () => {
 
-            for (let i = 0; i < capacity; i++) {
+        for (let i = 0; i < capacity; i++) {
 
-                const id =
-                    (this.cursor + i) % capacity
+            const id = (pool.cursor + i) % capacity
 
+            if (!pool.alive[id]) {
 
-                if (this.alive[id] === 0) {
+                pool.alive[id] = 1
+                pool.cursor = (id + 1) % capacity
 
-                    this.alive[id] = 1
-
-                    this.cursor =
-                        (id + 1) % capacity
-
-                    return id
-                }
-
+                return id
             }
-
-
-            return -1
-        },
-
-
-        kill(id) {
-
-            this.alive[id] = 0
-
-        },
-
-
-        clear() {
-
-            this.alive.fill(0)
 
         }
 
+        return -1
     }
 
-    // create custom attributes
+    pool.kill = (id) => {
+        pool.alive[id] = 0
+    }
+
+    pool.clear = () => {
+        pool.alive.fill(0)
+    }
+
     for (const name of fields) {
         pool[name] = new Float32Array(capacity)
     }
+
     return pool
 }
